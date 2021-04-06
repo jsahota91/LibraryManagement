d06ff835f3d6df067a60526e5268e29343fd127f
jsahota91 <jsahota@spartaglobal.com>
2021-04-06 20:38:13 +0100

    style changes

--- ResetIdQuery.sql
+++ ResetIdQuery.sql
@@ -1,4 +1,4 @@
-﻿DBCC CHECKIDENT('LibraryManagement.dbo.Books', RESEED, 9)
+﻿DBCC CHECKIDENT('LibraryManagement.dbo.Books', RESEED, 8)
 DBCC CHECKIDENT('LibraryManagement.dbo.Genres', RESEED, 7)
 DBCC CHECKIDENT('LibraryManagement.dbo.Authors', RESEED, 7)
-DBCC CHECKIDENT('LibraryManagement.dbo.Locations', RESEED, 6)
+DBCC CHECKIDENT('LibraryManagement.dbo.Locations', RESEED, 8)
